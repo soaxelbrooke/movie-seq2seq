3aff1b1d29c651432f102f59fd3ca36b6cfd9db2
soaxelbrooke <stuart@axelbrooke.com>
2017-09-19 16:47:45 +0900

    Add develop data loading and train/test split

--- train.py
+++ train.py
@@ -1,6 +1,54 @@
 
+import os
+import random
+
+pct_test_data = 0.20
+
 
 def train():
     """ Train keras model and save to disk in models/latest.bin """
+    print("Loading development data...")
+    develop_data = load_develop_data()
+    train_in, train_out, test_in, test_out = train_test_split(develop_data)
+    print("Selected {} training examples and {} test examples...".format(
+        len(train_in), len(test_in)))
 
 
+
+def train_test_split(develop_data):
+    test_movie_ids = set(random.sample(develop_data.keys(), int(len(develop_data)*pct_test_data)))
+
+    train_in, train_out = [], []
+    test_in, test_out = [], []
+    for movie_id, (input_data, output_data) in develop_data.items():
+        if movie_id in test_movie_ids:
+            test_in.extend(input_data)
+            test_out.extend(output_data)
+        else:
+            train_in.extend(input_data)
+            train_out.extend(output_data)
+
+    return train_in, train_out, test_in, test_out
+
+
+def load_develop_data(path='data/develop/'):
+    fnames = [fname for fname in os.listdir(path) if fname.startswith('m')]
+    movie_ids = set(fname.split('_')[0] for fname in fnames)
+
+    def read_movie(movie_id):
+        with open('{}/{}_in.txt'.format(path.rstrip('/'), movie_id), 'rb') as infile:
+            in_lines = list(map(bytes.strip, infile))
+        with open('{}/{}_out.txt'.format(path.rstrip('/'), movie_id), 'rb') as infile:
+            out_lines = list(map(bytes.strip, infile))
+        return in_lines, out_lines
+
+    return {movie_id: read_movie(movie_id) for movie_id in movie_ids}
+
+
+def build_model():
+    """ Build keras model """
+    raise NotImplementedError
+
+
+def vocab_for_lines():
+    """ Build vocab for provided training/test data """
