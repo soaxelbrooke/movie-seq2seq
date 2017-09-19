fd7aaa2d28e4346672999aeb0fdd385d3d3a5ba4
soaxelbrooke <stuart@axelbrooke.com>
2017-09-19 16:19:00 +0900

    Add training mode

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 
 import sys
 from prep import prep
+from train import train
 
 
 def main(mode):
     if mode == 'prep':
         prep()
-
+    if mode == 'train':
+        train()
 
 if __name__ == '__main__':
     main(sys.argv[1])
---
+++ train.py
@@ -0,0 +1,6 @@
+
+
+def train():
+    """ Train keras model and save to disk in models/latest.bin """
+
+
