eee1d1979b4ba99e182f8ccf9922f418de7dd18e
soaxelbrooke <stuart@axelbrooke.com>
2017-09-26 19:12:29 +0900

    Clean up code a lot, get attention... "working"

--- train.py
+++ train.py
@@ -1,12 +1,13 @@
 
 import os
 import random
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.preprocessing import label_binarize
-from collections import defaultdict
+
 import numpy as np
-import toolz
 from bpe.encoder import Encoder
+from keras import Input
+from keras.engine import Model
+from keras.layers import Concatenate, Embedding, LSTM, Dense, TimeDistributed, Lambda, Multiply, \
+    Add, Reshape, concatenate, Dot
 
 PCT_TEST_DATA = 0.20
 VOCAB_SIZE = 2**12
@@ -21,18 +22,21 @@ LSTM_RECURRENT_DROPOUT = 0.2
 DENSE_DROPOUT = 0.2
 
 USE_GLOVE = False
+USE_ATTENTION = False
 
 META_BATCH_SIZE = 1024 * BATCH_SIZE
 
 
 START = '<start/>'
-PAD = '<pad/>'
 END = '<end/>'
 OOV = '<oov/>'
 
 
 def train():
     """ Train keras model and save to disk in models/latest.bin """
+    print("Building keras seq2seq model...")
+    model = build_model()
+
     print("Loading development data...")
     develop_data = load_develop_data()
     train_in, train_out, test_in, test_out = train_test_split(develop_data)
@@ -41,68 +45,89 @@ def train():
 
     print("Fitting vocab from loaded data...")
     encoder = encoder_for_lines(train_in + train_out + test_in + test_out)
-    # vocab, tokenize = vocab_for_lines(train_in + train_out + test_in + test_out)
-    # inverse_vocab = {v: k for k, v in vocab.items()}
-    # print(tokenize(train_in[0]))
-    print(train_in[0:1])
-    print(list(encoder.transform(train_in[0:1])))
-    print(list(encoder.inverse_transform(encoder.transform(train_in[0:1]))))
 
     print("Transforming input and output data...")    
     train_x = encode_data(encoder, train_in, is_input=True)
     train_y = encode_data(encoder, train_out, is_input=False)
     test_x = encode_data(encoder, test_in, is_input=True)
     test_y = encode_data(encoder, test_out, is_input=False)
-    print(train_x.shape, train_y.shape, test_x.shape, test_y.shape)
-    print(train_x)
-    print(train_y)
-    print(test_x)
-    print(test_y)
-
-    # train_x = vectorize_data(tokenize, vocab, train_in, is_input=True)
-    # train_y = vectorize_data(tokenize, vocab, train_out, is_input=False)
-    # test_x = vectorize_data(tokenize, vocab, test_in, is_input=True)
-    # test_y = vectorize_data(tokenize, vocab, test_out, is_input=False)
-    # print(train_x.shape, train_y.shape, test_x.shape, test_y.shape)
-    
-    print("Building keras seq2seq model...")
-    # model = build_model(vocab)
-    model = build_model(None)
+    encoder.mute()
 
     print("Summary of built model:")
     print(model.summary())
 
     print("Training model...")
-    # train_model(model, vocab, inverse_vocab, tokenize, train_x, train_y, test_x, test_y)
-    train_model_2(model, encoder, train_x, train_y, test_x, test_y)
+    train_model(model, encoder, train_x, train_y, test_x, test_y)
 
     model.save('latest_model.h5')
 
     while True:
         print("Reply to what?")
         text = input('> ')
-        # print(decode_input_text(model, vocab, inverse_vocab, tokenize, text))
-        print(decode_input_text_2(model, encoder, text))
+        print(decode_input_text(model, encoder, text))
 
 
-def build_model(vocab):
-    from keras.layers import Input, Embedding, LSTM, Dense, concatenate, Reshape, Flatten, \
-        RepeatVector, Dropout, GRU, Bidirectional, add, Activation, Lambda, dot, multiply
-    from keras.layers.wrappers import TimeDistributed
-    from keras.models import Model
-    from keras.activations import softmax
-    from keras import backend as K
+def build_model_2():
+    shared_embedding = Embedding(
+        output_dim=WORD_EMBED_SIZE,
+        input_dim=VOCAB_SIZE,
+        input_length=MAXLEN_INPUT,
+        name='embedding',
+    )
 
-    # Shared embedding used by encoder and decoder input tokens.  GloVe weight initialization is
-    # optional, but it was found to not impact the results meaningfully or increase the speed of
-    # training.
+    encoder_input = Input(
+        shape=(MAXLEN_INPUT, ),
+        dtype='int32',
+        name='encoder_input',
+        batch_shape=(BATCH_SIZE, MAXLEN_INPUT),
+    )
+    print(encoder_input.shape)
+
+    embedded_input = shared_embedding(encoder_input)
+    print(embedded_input.shape)
+
+    encoder_output, encoder_state = LSTM(
+        CONTEXT_SIZE,
+        name='encoder_0',
+        dropout=LSTM_OUTPUT_DROPOUT,
+        recurrent_dropout=LSTM_RECURRENT_DROPOUT,
+        return_sequences=False,
+        return_state=True,
+    )(embedded_input)
+
+    decoder_output, decoder_state = LSTM(
+        CONTEXT_SIZE,
+        name='decoder_0',
+        dropout=LSTM_OUTPUT_DROPOUT,
+        recurrent_dropout=LSTM_RECURRENT_DROPOUT,
+        return_sequences=True,
+        return_state=True,
+    )(encoder_input, encoder_state)
+
+    word_dists = TimeDistributed(
+        Dense(
+            VOCAB_SIZE,
+            activation='softmax',
+            name='word_dist',
+        )
+    )(decoder_output)
+
+    model = Model(inputs=[encoder_input], outputs=word_dists)
+    print(model.summary())
+    return model
+
+
+def build_model():
+    """ Shared embedding used by encoder and decoder input tokens.  GloVe weight initialization is
+        optional, but it was found to not impact the results meaningfully or increase the speed of
+        training.
+    """
     # Shape of embeddings are (N, WORD_EMBED_SIZE)
     shared_embedding = Embedding(
         output_dim=WORD_EMBED_SIZE,
         input_dim=VOCAB_SIZE,
         input_length=1,
         name='embedding',
-        weights=[load_embedding_glove_weights(vocab)] if USE_GLOVE else None,
     )
 
     # =============================================================================================
@@ -141,7 +166,9 @@ def build_model(vocab):
         )(embedded_input)
     )
 
-    for input_idx in range(1, MAXLEN_INPUT):
+    encoder_outputs = []
+
+    for input_idx in range(0, MAXLEN_INPUT):
         # We have to use a Lambda layer here to slice the (MAXLEN_INPUT, WORD_EMBED_SIZE)-shaped
         # input tensor into (1, WORD_EMBED_SIZE)-shaped tensors for each LSTM invocation.  We have
         # to tell Keras the shape of the result, since it can't be inferred from the lambda inside.
@@ -155,6 +182,8 @@ def build_model(vocab):
             encoder_input_slice,
             current_encoder_state,
         )
+
+        encoder_outputs.append(current_encoder_output)
 
     # At this point, our encoder is complete.  The `current_encoder_state` is two tensors that
     # represent the LSTM state after the last token has been fed in, which we need to intiialize
@@ -177,14 +206,14 @@ def build_model(vocab):
 
     # Inputs, embedding, and decoder LSTM creation are unchanged from the encoder stage.
 
-    decoder_input = Input(
+    decoder_inputs = Input(
         shape=(MAXLEN_INPUT, ),
         dtype='int32',
-        name='decoder_input_0',
+        name='decoder_inputs_0',
         batch_shape=(BATCH_SIZE, MAXLEN_INPUT),
     )
 
-    embedded_decoder_input = shared_embedding(decoder_input)
+    embedded_decoder_input = shared_embedding(decoder_inputs)
 
     decoder_lstm = LSTM(
         CONTEXT_SIZE,
@@ -201,19 +230,65 @@ def build_model(vocab):
     current_decoder_state = current_encoder_state
     decoder_outputs = []
 
+    attn_dot = Dot(axes=(2, 1), normalize=True)
+    attn_cat = Concatenate(axis=1, input_shape=(1,), name='attn_cat')
+    attn_softmax = Dense(MAXLEN_INPUT, input_shape=(MAXLEN_INPUT, ), activation='softmax',
+                         name='attn_softmax')
+    attn_multiply = Multiply(name='attn_multiply')
+    attn_sum = Add(input_shape=(CONTEXT_SIZE, ), name='attn_sum')
+
+    def attend_to(decoder_state):
+        # Dot product result shape: (BATCH_SIZE, 1) per decoder input
+        _dots = [
+            attn_dot([
+                Reshape((1, CONTEXT_SIZE))(decoder_state),
+                Reshape((CONTEXT_SIZE, 1))(encoder_outputs[idx]),
+            ])
+            for idx in range(MAXLEN_INPUT)
+        ]
+        # Concatenation for the attention softmax, new shape (BATCH_SIZE, MAXLEN_INPUT)
+        _cat = Reshape((MAXLEN_INPUT,))(attn_cat(_dots))
+        _softmax = attn_softmax(_cat)
+        # Softmax provides weights per encoder output, need to split with lambda
+        _multiplies = [
+            attn_multiply([
+                encoder_outputs[idx],
+                Lambda(
+                    lambda x: x[:, idx:idx+1],
+                    output_shape=(MAXLEN_INPUT, CONTEXT_SIZE),
+                )(_softmax)
+            ])
+            for idx in range(MAXLEN_INPUT)
+        ]
+
+        _sum = attn_sum(_multiplies)
+        # Lambda required here to show Keras the result layer shape
+        return Lambda(
+                    lambda x: x,
+                    output_shape=(1, CONTEXT_SIZE),
+                )(_sum)
+
     for decoder_idx in range(0, MAXLEN_INPUT):
         # Same slicing of inputs and passing of last decoder state into next decoder invoation that
         # we saw in the endocer above.  If attention was added, it would be concatenated with the
         # decoder input here.
         decoder_input_slice = \
             Lambda(
-                lambda x: x[:, decoder_idx:decoder_idx+1], 
+                lambda x: x[:, decoder_idx],
                 output_shape=(1, WORD_EMBED_SIZE),
             )(embedded_decoder_input)
 
+        if USE_ATTENTION:
+            decoder_input = concatenate([
+                decoder_input_slice,
+                attend_to(current_decoder_state[0])
+            ])
+        else:
+            decoder_input = decoder_input_slice
+
         current_decoder_output, *current_decoder_state = \
             decoder_lstm(
-                decoder_input_slice,
+                Reshape((CONTEXT_SIZE + WORD_EMBED_SIZE, 1))(decoder_input),
                 current_decoder_state,
             )
 
@@ -232,15 +307,13 @@ def build_model(vocab):
         TimeDistributed(
             Dense(
                 VOCAB_SIZE,
-                # activation=lambda layer: softmax(layer, axis=1),
                 activation='softmax',
                 name='word_dist',
             )
         )(decoder_outputs)
 
-    model = Model(inputs=[encoder_input, decoder_input], outputs=word_dists)
+    model = Model(inputs=[encoder_input, decoder_inputs], outputs=word_dists)
     return model
-
 
 
 def train_model_epoch(model, train_x, train_y, test_x, test_y, N, START):
@@ -251,7 +324,6 @@ def train_model_epoch(model, train_x, train_y, test_x, test_y, N, START):
     ])
     # We have to add an extra dimension for Keras' sparse_categorical_crossentropy
     answers_out_train = train_y.reshape((len(train_x), MAXLEN_INPUT, 1))
-
 
     answers_in_test = np.hstack([
         np.ones((len(test_x), 1), dtype='int32') * START,
@@ -279,14 +351,16 @@ def train_model_epoch(model, train_x, train_y, test_x, test_y, N, START):
     print('\nValidation score:', score)
 
 
-def train_model_2(model, encoder, train_x, train_y, test_x, test_y):
+def train_model(model, encoder, train_x, train_y, test_x, test_y):
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
 
-    start_idx = encoder.word_vocab[START + encoder.EOW]
+    start_idx = encoder.word_vocab[START]
 
     TEST_INPUT = [
         "__romance When did you start drinking again?",
-        "__thriller So you're from around here?",
+        "__crime So you're from around here?",
+        "__thriller Oh god, what do we do?",
+        "__romance Do you love me?",
     ]
 
     try:
@@ -297,7 +371,7 @@ def train_model_2(model, encoder, train_x, train_y, test_x, test_y):
             print("Evaluating test examples...")
             for text in TEST_INPUT:
                 print('> ', text)
-                print(decode_input_text_2(model, encoder, text))
+                print(decode_input_text(model, encoder, text))
 
             train_model_epoch(model, train_x, train_y, test_x, test_y, N, start_idx)
 
@@ -306,30 +380,14 @@ def train_model_2(model, encoder, train_x, train_y, test_x, test_y):
         return model
 
 
-def decode_input_text(model, vocab, inverse_vocab, tokenize, text):
+def decode_input_text(model, encoder, text):
     """ Horrible hack of a function to test responses to arbitrary strings. Don't try this at home,
         kids.
     """
-    result_tokens = [vocab[START]]
-    for i in range(MAXLEN_INPUT):
-        answer_vec = (result_tokens + [vocab[START]] * MAXLEN_INPUT)[:MAXLEN_INPUT]
-        input_vec = [vectorize_data(tokenize, vocab, [text], is_input=True), 
-                     np.array(answer_vec).reshape((1, MAXLEN_INPUT))]
-        prediction = model.predict([np.vstack([vec]*BATCH_SIZE) 
-                                    for vec in input_vec])[0].reshape(MAXLEN_INPUT, VOCAB_SIZE)
-        tokens = list(prediction.argmax(axis=1))
-        result_tokens.append(tokens[i])
-    return ' '.join([inverse_vocab[idx] for idx in result_tokens])
-
-
-def decode_input_text_2(model, encoder, text):
-    """ Horrible hack of a function to test responses to arbitrary strings. Don't try this at home,
-        kids.
-    """
-    result_tokens = [encoder.word_vocab[START + encoder.EOW]]
+    result_tokens = [encoder.word_vocab[START]]
     for i in range(MAXLEN_INPUT):
         answer_vec = (result_tokens + 
-                      [encoder.word_vocab[PAD + encoder.EOW]] * MAXLEN_INPUT)[:MAXLEN_INPUT]
+                      [encoder.bpe_vocab[encoder.PAD]] * MAXLEN_INPUT)[:MAXLEN_INPUT]
         input_vec = [encode_data(encoder, [text], is_input=True), 
                      np.array(answer_vec).reshape((1, MAXLEN_INPUT))]
         prediction = model.predict([np.vstack([vec]*BATCH_SIZE) 
@@ -369,77 +427,15 @@ def load_develop_data(path='data/develop/'):
     return {movie_id: read_movie(movie_id) for movie_id in movie_ids}
 
 
-def vectorize_data(tokenize, vocab, lines, is_input=False):
-    vectors = []
-
-    for tokens in map(tokenize, lines):
-        vector = []
-
-        for idx in range(MAXLEN_INPUT):
-            if idx < len(tokens):
-                vector.append(vocab[tokens[idx]])
-            else:
-                vector.append(vocab[PAD])
-
-        if is_input:
-            vectors.append(vector[::-1])
-        else:
-            vectors.append(vector)
-
-    return np.array(vectors)
-
-
-def vocab_for_lines(lines):
-    """ Build vocab for provided training/test data """
-    vectorizer = CountVectorizer(max_features=VOCAB_SIZE - 4, stop_words=[], token_pattern='\w+')
-    vectorizer.fit(lines)
-    vocab = vectorizer.vocabulary_
-    vocab[PAD] = len(vocab)
-    vocab[START] = len(vocab)
-    vocab[END] = len(vocab)
-    vocab[OOV] = len(vocab)
-
-    return defaultdict(lambda: vocab[OOV], vocab.items()), vectorizer.build_analyzer()
-
-
-def load_embedding_glove_weights(vocab):
-    print("Loading glove weights...")
-    with open('glove/glove.twitter.27B.{}d.txt'.format(WORD_EMBED_SIZE)) as infile:
-        line_iter = (line.strip().split(' ') for line in infile)
-        word_embeds = {word: list(map(float, rest)) for word, *rest in line_iter if word in vocab}
-
-    inverse_vocab = {v: k for k, v in vocab.items()}
-    sorted_words = [inverse_vocab[idx] for idx in range(VOCAB_SIZE)]
-    def default():
-        return list(np.random.rand(WORD_EMBED_SIZE))
-
-    results = []
-
-    hits = 0
-    for word in sorted_words:
-        if word in word_embeds:
-            hits += 1
-            results.append(word_embeds[word])
-        else:
-            results.append(default())
-
-    array_result = np.array(results, dtype='float32')
-    print("{} glove hits out of {} words in vocab ({}%)".format(
-        hits, VOCAB_SIZE, 100 * hits / VOCAB_SIZE))
-    assert array_result.shape == (VOCAB_SIZE, WORD_EMBED_SIZE)
-    return array_result
-
-
 def encode_data(encoder, text, is_input=False):
     """ Encode data using provided encoder, for use in training/testing """
-    return np.array(list(encoder.transform(text, reversed=is_input, fixed_length=MAXLEN_INPUT,
-                                           padding=PAD)),
+    return np.array(list(encoder.transform(text, reverse=is_input, fixed_length=MAXLEN_INPUT)),
                     dtype='int32')
 
 
 def encoder_for_lines(lines):
     """ Calculate BPE encoder for provided lines of text """
-    encoder = Encoder(vocab_size=VOCAB_SIZE, required_tokens=[START, PAD])
+    encoder = Encoder(vocab_size=VOCAB_SIZE, required_tokens=[START])
     encoder.fit(lines)
     encoder.save('latest_encoder.json')
     return encoder
